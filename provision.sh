3623cf83456483864fc11f5b4682d2de8632ce20
Benjamin Fischer <benjamin.fischer@evolutics.info>
2026-01-25 22:29:50 +0100

    Configure keyboard layout earlier for debugging if other commands break

--- provision.sh
+++ provision.sh
@@ -2,6 +2,11 @@
 
 set -o errexit -o nounset -o pipefail -x
 
+sudo sed \
+  --expression 's/^XKBLAYOUT=.*/XKBLAYOUT="de,us"/' \
+  --expression 's/^XKBVARIANT=.*/XKBVARIANT="neo,"/' \
+  --in-place /etc/default/keyboard
+
 sudo apt-get update
 sudo apt-get --yes install gnome-session gnome-terminal spice-vdagent
 
@@ -9,11 +14,6 @@ sudo snap install firefox
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get --yes dist-upgrade
 sudo snap refresh
-
-sudo sed \
-  --expression 's/^XKBLAYOUT=.*/XKBLAYOUT="de,us"/' \
-  --expression 's/^XKBVARIANT=.*/XKBVARIANT="neo,"/' \
-  --in-place /etc/default/keyboard
 
 sudo rsync --archive --mkpath --verbose wormhole/firefox_policies.json \
   /etc/firefox/policies/policies.json
