3e50d1b316455b2df0e19859305a277c299a65a2
Benjamin Fischer <benjamin.fischer@evolutics.info>
2026-03-05 21:31:16 +0100

    Separate building image more clearly from running VM

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -3,9 +3,9 @@
 set -o errexit -o nounset -o pipefail
 
 build_image() {
-  VM_PASSWORD="$(openssl rand -base64 32)"
-  export VM_PASSWORD
-  PKR_VAR_password_hash="$(openssl passwd -6 -- "${VM_PASSWORD}")"
+  local password
+  password="$(openssl rand -base64 32)"
+  PKR_VAR_password_hash="$(openssl passwd -6 -- "${password}")"
   export PKR_VAR_password_hash
 
   export PKR_VAR_ssh_private_key_file=.ssh/id
@@ -16,19 +16,27 @@ build_image() {
     packer init .
     packer build -on-error=ask .
   )
+
+  local output_folder
+  output_folder="$(query_last_build_manifest '.custom_data.output_folder')"
+  (
+    umask u=rw,go=
+    printf '%s' "${password}" >"${output_folder}/password.txt"
+  )
+}
+
+query_last_build_manifest() {
+  jq \
+    '. as $root | .builds[] | select(.packer_run_uuid == $root.last_run_uuid)' \
+    packer-manifest.json \
+    | jq --raw-output "$1"
 }
 
 run_vm() {
-  local build
-  build="$(jq \
-    '. as $root | .builds[] | select(.packer_run_uuid == $root.last_run_uuid)' \
-    packer-manifest.json)"
-
-  local iso_version output_file output_folder share_name
-  iso_version="$(echo "${build}" | jq --raw-output '.custom_data.iso_version')"
-  output_file="$(echo "${build}" | jq --raw-output '.files[0].name')"
-  output_folder="$(echo "${build}" | jq --raw-output '.custom_data.output_folder')"
-  share_name="$(echo "${build}" | jq --raw-output '.custom_data.share_name')"
+  local iso_version output_file share_name
+  iso_version="$(query_last_build_manifest '.custom_data.iso_version')"
+  output_file="$(query_last_build_manifest '.files[0].name')"
+  share_name="$(query_last_build_manifest '.custom_data.share_name')"
 
   local -r memory_in_mib=8192
 
@@ -42,11 +50,6 @@ run_vm() {
     --noautoconsole \
     --os-variant "ubuntu${iso_version:0:5}" \
     --vcpus 4
-
-  (
-    umask u=rw,go=
-    printf '%s' "${VM_PASSWORD}" >"${output_folder}/password.txt"
-  )
 }
 
 main() {
